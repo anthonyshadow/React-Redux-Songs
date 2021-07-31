90c5b00d5a898be86a16ccfb014ed287d06b2deb
anthonyshadow <anthony.shadowitz@gmail.com>
2021-07-31 13:20:17 -0400

    create a refactored functional component for SongList

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { selectSong } from '../actions';
 
+//Class Component
+
 class SongList extends Component {
     renderList() {
         return this.props.songs.map((song) => {
@@ -25,6 +27,32 @@ class SongList extends Component {
         return <div className="ui divided list">{this.renderList()}</div>
     }
 };
+
+
+//Refactored to a functional Component
+
+// const SongList = ({songs}) => {
+
+    
+//     const renderedList = songs.map((song) => {
+//         console.log(song)
+//         console.log(selectSong(song))
+
+//             return (
+//                 <div className='item' key={song.title}>
+//                     <div className="right floated content">
+//                         <button 
+//                         className="ui button primary"
+//                         onClick={(song) => selectSong(song)}>
+//                             Select
+//                         </button>
+//                     </div>
+//                     <div className="content">{song.title}</div>
+//                 </div>
+//             );
+//         });
+//     return <div className="ui divided list">{renderedList}</div>
+// };
 
 const mapStateToProps = (state) => {
     console.log(state);
