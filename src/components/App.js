43407bda47bc0bb20cb8b8a293f9b4cb67ea842b
anthonyshadow <anthony.shadowitz@gmail.com>
2021-07-31 12:56:55 -0400

    add song detail component and render selected songs from redux store

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SongList from './SongList'
+import SongDetail from './SongDetail'
 
 const App = () => {
   return ( 
@@ -8,7 +9,10 @@ const App = () => {
         <div className="ui row">
             <div className="column eight wide">
                 <SongList />
-            </div>          
+            </div>
+            <div className="column eight wide">
+                <SongDetail />
+            </div>              
         </div>   
     </div>);  
 };
